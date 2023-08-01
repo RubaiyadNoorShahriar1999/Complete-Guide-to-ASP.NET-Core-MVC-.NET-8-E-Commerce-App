20a7f03cce28de771f2d012397f12803ab1e8ec9
RubaiyadNoorShahriar1999 <rubaiyadnoorshahriar@gmail.com>
2023-08-01 13:45:53 +0600

    Done till video 29

---
+++ Bulky/BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
