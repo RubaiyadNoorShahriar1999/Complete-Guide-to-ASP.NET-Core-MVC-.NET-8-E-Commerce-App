5d19bf730c5cb7bf76dbaff42ca5ccafc571bc01
RubaiyadNoorShahriar1999 <rubaiyadnoorshahriar@gmail.com>
2023-08-01 19:29:03 +0600

    Done till video 35

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -15,7 +15,7 @@ namespace BulkyWeb.Controllers
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
-            return View();
+            return View(objCategoryList);
         }
     }
 }
